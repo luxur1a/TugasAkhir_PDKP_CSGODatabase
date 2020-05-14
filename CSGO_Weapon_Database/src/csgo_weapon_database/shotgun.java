d59812796870e1825d4e67834d528646b7940da1
Benediktus Gilang <57548941+luxur1a@users.noreply.github.com>
2020-05-14 15:44:36 +0700

    Create shotgun.java

---
+++ CSGO_Weapon_Database/src/csgo_weapon_database/shotgun.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csgo_weapon_database;
+
+/**
+ *
+ * @author Gil
+ */
+public class shotgun {
+    
+    String[][] weapon = {
+        {"Mag-7","$1,300","$900","30/bullet (8 bullets)","75%","70.59","225","3.24m","62.99","97.09","308.52","114.16","N/A"},
+        {"Nova","$1,050","$900","26/bullet (9 bullets)","50%","68.18","220","3.24m","83.75","173.31","197.50","54.98","N/A"},
+        {"Sawed-Off","$1,100","$900","32/bullet (8 bullets)","75%","70.59","210","2.21m","85.80","126.70","134","154.58","N/A"},
+        {"XM1014","$2,000","$900","20/bullet (6 bullets)","80%","171.43","215","3.39m","81.03","175.83","192.42","N/A","N/A"},
+              
+        
+    };
+    
+}

fd827a775dd5be57b8eb1daecf7f29e96f80fa56
Benediktus Gilang <57548941+luxur1a@users.noreply.github.com>
2020-05-14 15:44:27 +0700

    Create sniper.java

---
+++ CSGO_Weapon_Database/src/csgo_weapon_database/sniper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csgo_weapon_database;
+
+/**
+ *
+ * @author Gil
+ */
+public class sniper {
+    
+    String[][] weapon = {
+        {"AWP","4,750","$100","115","97.5%","41.24","225","1.88m || Scoped : 69.27m","257.48 || Scoped : 178.68","214.83 || Scoped : 136.03","273.20","75,920.66","74,661.11"},
+        {"G3SG1","$5,000","$300","80","82.50%","240","220","5.84m || Scoped 66.26m","176.58 || Scoped : 152.78","179.87 || Scoped : 156.07","233.08","28,787","24,025.96"},
+        {"SCAR-20","$5,000","$300","80","82.5%","240","210","5.84m || Scoped : 66.26m","176.58 || Scoped : 152.78","179.87 || Scoped : 156.07","233.08","28,787","24,025.96"},
+        {"SSG 08","$1,700","$300","88","85%","48","215","4.77m || Scoped : 47.18m","155.43 || Scoped : 126.68","37.70 || Scoped : 8.95","191.26","31,145.85","27,123.64"},
+              
+        
+    };
+    
+}

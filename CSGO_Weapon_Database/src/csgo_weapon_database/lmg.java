12da029fd28af444815445988d550405917d655b
Benediktus Gilang <57548941+luxur1a@users.noreply.github.com>
2020-05-14 15:45:49 +0700

    batch upload

---
+++ CSGO_Weapon_Database/src/csgo_weapon_database/lmg.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csgo_weapon_database;
+
+/**
+ *
+ * @author Gil
+ */
+public class lmg {
+    
+    
+    String[][] weapon = {
+        {"M249","$5,200","$300","32","80%","750","195","15.71m","165.95","289.17","267.62","4,052.31","389.32"},
+        {"Negev","$1,700","$300","35","71%","800","150","12.52m","171.31","304.40","274.86","5,523.33","N/A"},
+              
+        
+    };
+    
+}
